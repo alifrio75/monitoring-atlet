dfe3097a2df317e31b7a85b4534eaf3b385ab6f2
alifrio75 <alifrio75@gmail.com>
2018-05-17 14:32:18 +0700

    Add Reactstrap
    reactstrap

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import App from './components/App'
-import './styles/normalize.css'
-import './styles/layout.css'
-import './styles/animation.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
+import "./styles/normalize.css";
+import "./styles/layout.css";
+import "./styles/animation.css";
 
-ReactDOM.render((
+ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
-), document.getElementById('app'))
-
-//commit
+  </BrowserRouter>,
+  document.getElementById("app")
+);
